851a5109448d275694c94b8dfdc95ee6c23b8af5
Lee1387 <127062297+Lee1387@users.noreply.github.com>
2023-07-03 21:44:58 +0100

    ChatCommand.php now done

---
+++ src/Lee1387/Guardian/commands/ChatCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Lee1387\Guardian\commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use Lee1387\Guardian\session\SessionFactory;
+use Lee1387\Guardian\utils\Permissions;
+use Lee1387\Guardian\utils\Prefixes;
+
+class ChatCommand extends Command {
+
+    public function __construct()
+    {
+        parent::__construct("staffchat");
+
+        $this->setPermission(Permissions::CHAT_COMMAND);
+
+        $this->setAliases(["sc"]);
+
+        $this->setDescription("Guardian Chat Command");
+    }
+
+    public function execute(CommandSender $player, string $commandLabel, array $args): void 
+    {
+        if (!$player instanceof Player) return;
+
+        if (!$this->testPermission($player)) {
+            $player->sendMessage(Prefixes::PLUGIN . "You don't have permission to use this.");
+            return;
+        }
+
+        if (!SessionFactory::isChat($player)) {
+            SessionFactory::sendChat($player);
+            $player->sendMessage(Prefixes::PLUGIN . "You have entered into StaffChat.");
+            return;
+        }
+
+        SessionFactory::cancelChat($player);
+        $player->sendMessage(Prefixes::PLUGIN . "You have exited StaffChat.");
+    }
+}
